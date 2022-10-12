ea5d89470b7f9ad39111c729babf76542b673fac
janakbista <bistajanak303@gmail.com>
2022-10-12 17:27:55 +0545

    ImagePicker and ColorPicker Implemented

--- app/src/main/java/com/example/memesansar/MainActivity.kt
+++ app/src/main/java/com/example/memesansar/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.memesansar
 
+import android.app.Activity
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -13,16 +16,27 @@ import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.navigation.NavigationView
+import yuku.ambilwarna.AmbilWarnaDialog
 
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     lateinit var drawerLayout: DrawerLayout
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     lateinit var navigationView: NavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var bgImagepicker = findViewById<ImageView>(R.id.imagepicker)
+        var bgColorpicker = findViewById<ImageView>(R.id.bgcolorpicker)
+
+        bgImagepicker.setOnClickListener {
+            openGalleryForImage()
+        }
+        bgColorpicker.setOnClickListener {
+            openColorPickerDialogue()
+        }
+
 
         drawerLayout = findViewById(R.id.my_drawer_layout)
         actionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close)
@@ -69,6 +83,39 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+         var REQUEST_CODE = 200
+        startActivityForResult(intent,REQUEST_CODE)
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val REQUEST_CODE = 200
+        var imageView: ImageView = findViewById<ImageView>(R.id.imageView)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+            imageView.setImageURI(data?.data) // handle chosen image
+        }
+    }
+
+    fun openColorPickerDialogue() {
+
+        var mDefaultColor = 0
+        var imageView: ImageView = findViewById<ImageView>(R.id.imageView)
+
+        val colorPickerDialogue = AmbilWarnaDialog(this, mDefaultColor,
+            object : AmbilWarnaDialog.OnAmbilWarnaListener {
+                override fun onCancel(dialog: AmbilWarnaDialog?) {
+                }
+
+                override fun onOk(dialog: AmbilWarnaDialog?, color: Int) {
+                    mDefaultColor = color
+                    imageView.setBackgroundColor(mDefaultColor)
+                }
+            })
+        colorPickerDialogue.show()
+    }
 }
 
