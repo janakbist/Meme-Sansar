2393ea430b263be61cc1ee964e2fd69869a1462f
janakbista <bistajanak303@gmail.com>
2022-10-18 09:25:13 +0545

    Text rotation and zooming done

--- app/src/main/java/com/example/memesansar/MainActivity.kt
+++ app/src/main/java/com/example/memesansar/MainActivity.kt
@@ -1,32 +1,43 @@
 package com.example.memesansar
 
 import android.app.Activity
-import android.content.ContentValues.TAG
 import android.content.Intent
+import android.graphics.PointF
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.animation.RotateAnimation
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.*
+import android.view.MotionEvent.INVALID_POINTER_ID
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.core.widget.addTextChangedListener
 import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationBarItemView
 import com.google.android.material.navigation.NavigationView
 import yuku.ambilwarna.AmbilWarnaDialog
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener {
     lateinit var drawerLayout: DrawerLayout
     lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     lateinit var navigationView: NavigationView
+
+    //text zooming and rotation required varibales
+    var lastEvent: FloatArray? = null
+    var d = 0f
+    var newRot = 0f
+    private var isZoomAndRotate = false
+    private var isOutSide = false
+    private val NONE = 0
+    private val DRAG = 1
+    private val ZOOM = 2
+    private var mode = NONE
+    private val start = PointF()
+    private val mid = PointF()
+    var oldDist = 1f
+    private var xCoOrdinate = 0f
+    private  var yCoOrdinate:kotlin.Float = 0f
+    //text zooming and rotation required variables
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,12 +46,26 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         var bgColorpicker = findViewById<ImageView>(R.id.bgcolorpicker)
         val insertText = findViewById<EditText>(R.id.insertText)
         var movableTextView = findViewById<TextView>(R.id.movableText)
+        val share = findViewById<Button>(R.id.shareBtn)
 
-
+        //show text change listener in text feild
         insertText.addTextChangedListener {
             val textData = insertText.text.toString()
             movableTextView.text = textData
+            share.setOnClickListener {
+                insertText.getText().clear()
+                movableTextView.text = textData
+            }
         }
+
+        movableTextView.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View, event: MotionEvent?): Boolean {
+                val view = v as TextView
+                view.bringToFront()
+                viewTransformation(view, event!!)
+                return true
+            }
+        })
 
         bgImagepicker.setOnClickListener {
             openGalleryForImage()
@@ -130,5 +155,128 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             })
         colorPickerDialogue.show()
     }
+    private fun viewTransformation(view: View, event: MotionEvent) {
+        when (event.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
+                xCoOrdinate = view.x - event.rawX
+                yCoOrdinate = view.y - event.rawY
+                start.set(event.x, event.y)
+                isOutSide = false
+                mode = DRAG
+                lastEvent = null
+            }
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                oldDist = spacing(event)
+                if (oldDist > 10f) {
+                    midPoint(mid, event)
+                    mode = ZOOM
+                }
+                lastEvent = FloatArray(4)
+                val also = event.getX(0).also { lastEvent!![0] = it }
+                event.getX(1).also { lastEvent!![1] = it }
+                lastEvent!![2] = event.getY(0)
+                lastEvent!![3] = event.getY(1)
+                d = rotation(event)
+            }
+            MotionEvent.ACTION_UP -> {
+                isZoomAndRotate = false
+                if (mode == DRAG) {
+                    val x = event.x
+                    val y = event.y
+                }
+                isOutSide = true
+                mode = NONE
+                lastEvent = null
+                mode = NONE
+                lastEvent = null
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                isOutSide = true
+                mode = NONE
+                lastEvent = null
+                mode = NONE
+                lastEvent = null
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+                mode = NONE
+                lastEvent = null
+            }
+            MotionEvent.ACTION_MOVE -> if (!isOutSide) {
+                if (mode == DRAG) {
+                    isZoomAndRotate = false
+                    view.animate().x(event.rawX + xCoOrdinate).y(event.rawY + yCoOrdinate)
+                        .setDuration(0).start()
+                }
+                if (mode == ZOOM && event.pointerCount == 2) {
+                    val newDist1 = spacing(event)
+                    if (newDist1 > 10f) {
+                        val scale: Float = newDist1 / oldDist * view.scaleX
+                        view.scaleX = scale
+                        view.scaleY = scale
+                    }
+                    if (lastEvent != null) {
+                        newRot = rotation(event)
+                        view.rotation = (view.rotation + (newRot - d)) as Float
+                    }
+                }
+            }
+        }
+    }
+
+    private fun rotation(event: MotionEvent): Float {
+        val delta_x = (event.getX(0) - event.getX(1)).toDouble()
+        val delta_y = (event.getY(0) - event.getY(1)).toDouble()
+        val radians = Math.atan2(delta_y, delta_x)
+        return Math.toDegrees(radians).toFloat()
+    }
+
+    private fun spacing(event: MotionEvent): Float {
+        val x = event.getX(0) - event.getX(1)
+        val y = event.getY(0) - event.getY(1)
+        return Math.sqrt((x * x + y * y).toDouble()).toInt().toFloat()
+    }
+
+    private fun midPoint(point: PointF, event: MotionEvent) {
+        val x = event.getX(0) + event.getX(1)
+        val y = event.getY(0) + event.getY(1)
+        point[x / 2] = y / 2
+    }
+
+    override fun onDown(p0: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onShowPress(p0: MotionEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLongPress(p0: MotionEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSingleTapConfirmed(p0: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDoubleTap(p0: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
 
